a91b8f8a4a2f3e7f213ff1c7e1b6fe158cac9cb2
pavelprfb <pavelprfb@gmail.com>
2025-05-14 12:55:39 +0600

    ok2

--- index.js
+++ index.js
@@ -30,6 +30,9 @@ function renderPage(code = "", userInput = "", error = "") {
   return `
   <html>
   <head>
+  	<meta charset="UTF-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  	<meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>2FA Generator</title>
     <style>
       body {
