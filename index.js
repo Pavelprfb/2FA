c1d6818f3340afa4dfa4df039885c5ab513471cc
pavelprfb <pavelprfb@gmail.com>
2025-05-14 19:30:10 +0600

    ok5

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
   res.render('index', { secret: '', otp: '' });
